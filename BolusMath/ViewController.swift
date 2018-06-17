512d959404466c71885a94c6265abd0ae7219e14
Ivo Viira <ivoviira@msn.com>
2018-06-17 14:19:17 +0300

    Initial Commit

---
+++ BolusMath/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BolusMath
+//
+//  Created by Ivo Viira on 17/06/2018.
+//  Copyright © 2018 Ivo Viira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
